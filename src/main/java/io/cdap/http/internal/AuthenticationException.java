96a3148d9855a0cab04cf4e82a03b8d4a903e46f
Robert <robert.engel98@web.de>
2019-11-18 20:12:27 +0100

    adding exceptions

---
+++ src/main/java/io/cdap/http/internal/AuthenticationException.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2017-2019 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.cdap.http.internal;
+
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.HttpVersion;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Exception that gets thrown when a request tries to access a secured resource
+ * and {@link io.cdap.http.AuthHandler#isAuthenticated(HttpRequest)} doesn't accept it.
+ */
+public class AuthenticationException extends HandlerException {
+
+    public AuthenticationException(String message) {
+        super(HttpResponseStatus.UNAUTHORIZED, message);
+    }
+
+    @Override
+    public HttpResponse createFailureResponse() {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.UNAUTHORIZED,
+                Unpooled.copiedBuffer("UNAUTHORIZED", StandardCharsets.UTF_8));
+        response.headers().add("WWW-Authenticate", getMessage());
+        HttpUtil.setContentLength(response, response.content().readableBytes());
+        return response;
+    }
+}
